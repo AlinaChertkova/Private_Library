74a12731616cdbaae95c69292130694d8d988223
Alina Chertkova <alinachertkova789@gmail.com>
2019-02-19 11:36:56 +0300

    First commit

---
+++ personalLib/src/main/java/com/example/personalLib/PersonalLibApplication.java
@@ -0,0 +1,13 @@
+package com.example.personalLib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalLibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalLibApplication.class, args);
+	}
+
+}
